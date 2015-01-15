fb04b4daad79aa09beb938fdff93451eb0ab88c9
Bailey Leising <bleising10@gmail.com>
2015-01-15 15:52:28 -0700

    first commit

--- toy-problems.js
+++ toy-problems.js
@@ -2,12 +2,19 @@
 
 /*
 
-Write a function that takes an array of integers and returns the sum of the integers after adding 1 to each.
+Write a function that takes an array of integers and returns the sum of the integers after adding 1 to 
+each.
 
 plusOneSum([1, 2, 3, 4]); // 14
 
 */
-
+var plus = function(arr){
+	var sum = 0;
+	for(var i = 0; i < arr.length; i++){
+		arr = arr[i] + 1;
+		sum.push(arr[i] * arr.length);
+	};
+};
 
 
 /*
@@ -22,14 +29,17 @@ flatten([1, 2, [3, [4], 5, 6], 7]) // [1, 2, 3, 4, 5, 6, 7]
 
 /*
 
-Given an array [a1, a2, ..., aN, b1, b2, ..., bN, c1, c2, ..., cN] convert it to [a1, b1, c1, a2, b2, c2, ..., aN, bN, cN]
+Given an array [a1, a2, ..., aN, b1, b2, ..., bN, c1, c2, ..., cN] convert it to 
+[a1, b1, c1, a2, b2, c2, ..., aN, bN, cN]
 
 */
 
 
 /*
 
-There is an array of non-negative integers. A second array is formed by shuffling the elements of the first array and deleting a random element. Given these two arrays, find which element is missing in the second array.
+There is an array of non-negative integers. A second array is formed by shuffling the elements of 
+the first array and deleting a random element. Given these two arrays, find which element is missing 
+in the second array.
 
 */
 
@@ -39,7 +49,8 @@ There is an array of non-negative integers. A second array is formed by shufflin
 
 /*
 
-Write a function that returns the longest word(s) from a sentence. The function should not return any duplicate words (case-insensitive).
+Write a function that returns the longest word(s) from a sentence. The function should not return any 
+duplicate words (case-insensitive).
 
 Example
 
@@ -52,7 +63,8 @@ longestWords("Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo") 
 
 /*
 
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum 
+of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 
@@ -61,7 +73,8 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 /*
 
-Remove duplicate characters in a given string keeping only the first occurrences. For example, if the input is ‘tree traversal’ the output will be "tre avsl".
+Remove duplicate characters in a given string keeping only the first occurrences. For example, if the 
+input is ‘tree traversal’ the output will be "tre avsl".
 
 */
 
