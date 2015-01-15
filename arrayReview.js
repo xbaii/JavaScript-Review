f2311f526bf649fc0bf95575f8afc24fed1153ff
Bailey Leising <bleising10@gmail.com>
2015-01-15 15:49:03 -0700

    first commit

--- arrayReview.js
+++ arrayReview.js
@@ -2,7 +2,10 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+var last = function() {
+  return threeItems[threeItems.length - 1];
+};
+
 
 
 
@@ -13,31 +16,48 @@ var threeItems = [1,2,3];
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
+var evenFinder = function() {
+  for (var i = 0; i < evenArray.length; i++) {
+    if (evenArray[i] % 2 !== 0) {
+      evenArray.splice(i, 1);
+      i--;
+    }
+  }
+};
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. 
+//Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. 
+//If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+var finder = function(array) {
+    if(array.indexOf(getRandomArbitrary()) !== -1) {
+      return true;
+    };
+    return false;
+  };
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this,
+// first should be just [1,2,3,4,5] and 
+//second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
 var second;
 
-  //code here
+var second = first.slice();
+second.push(6, 7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
@@ -51,7 +71,18 @@ alert(second) //[1,2,3,4,5,6,7];
 //write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
 var sentence = "Dev Mountain is the best"
 
-  //code here
+var longest = function(sentence) {
+	var st = sentence.split(" ");
+	var longest = 0;
+	var word = null;
+	for (var i = 0; i < str.length; i++) {
+		if (longest < str[i].length){
+			longest = str[i].length;
+			word = str[i];
+		}
+	}
+	return word;
+};
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -62,7 +93,16 @@ var sentence = "Dev Mountain is the best"
 var myPoem = 'What is a jQuery but a misunderstood object?'
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
-  //code here
+var capitalize = function(str) {
+  var strArr = str.split(" ");
+  var newArr = [];
+  for (var i = 0; i < strArr.length; i++) {
+    var FirstLetter = strArr[i].charAt(0).toUpperCase();
+    var restOfWord = strArr[i].slice(1);
+    newArr[i] = FirstLetter + restOfWord;
+  }
+  return newArr.join(' ');
+};
 
 
 
@@ -72,3 +112,29 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+var vowelCounter = function(str){
+	var vowels = 0;
+	var arr = str.split("");
+	for(var i = 0; i < arr.length; i++) {
+  		if(arr[i] === "a"){
+    		vowels += 1;
+  		}
+  		if(arr[i] === "e"){
+    		vowels += 1;
+  		}
+  		if(arr[i] === "i"){
+    		vowels += 1;
+  		}
+  		if(arr[i] === "o"){
+    		vowels += 1;
+  		}
+  		if(arr[i] === "u"){
+    		vowels += 1;
+  		}
+  	}
+  	return vowels;
+};
+
+
+
+
